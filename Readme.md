b8ce81dbe079eba137ba42afa17b12805b43ce8d
Dmitry Iovlev <iovlev.d@gmail.com>
2019-04-07 18:17:11 +0300

    First commit

---
+++ Readme.md
@@ -0,0 +1,13 @@
+# Flocktory TT
+
+## Installation
+
+`npm install`
+
+## Dev server
+
+`npm start`
+
+## Build
+
+`npm run build`
---
+++ src/js/script.js
@@ -0,0 +1,14 @@
+(function(){
+  var rulesToggle = document.querySelector('.js-rules-toggle');
+  var rulesText = document.querySelector('.js-rules-text');
+
+  rulesToggle.addEventListener('click', function(evt){
+    evt.preventDefault();
+    rulesToggle.classList.toggle('order-coupon__rules-toggle--open');
+    if(rulesText.style.display === 'block') {
+      rulesText.style = '';
+    } else {
+      rulesText.style.display = 'block'
+    }
+  });
+})();
